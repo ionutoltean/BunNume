be42e3375eccc13bd08fb02346cd7704e9f18ca2
Istvan Galpal <istvan.galpal@quadom.com>
2023-05-06 20:01:43 +0300

    Obstacle things

--- BunNume/Assets/Scripts/ObstacleSpawner.cs
+++ BunNume/Assets/Scripts/ObstacleSpawner.cs
@@ -6,18 +6,26 @@ using UnityEngine;
 public class ObstacleSpawner : MonoBehaviour
 {
     [SerializeField] private float spawnTimer = 10f;
+    [SerializeField] private float portalOpenTimer = 1f;
     public GameObject obstaclePrefab;
     public Transform spawnPointTransform;
     public Transform moveTowardsPointTransform;
+    public ParticleSystem portalParticle;
 
     private void Start()
     {
-        InvokeRepeating(nameof(SpawnOObstacle), spawnTimer,spawnTimer);
+        InvokeRepeating(nameof(SpawnParticle), spawnTimer,spawnTimer);
     }
-
-    private void SpawnOObstacle()
+    
+    private void SpawnParticle()
+    {
+        portalParticle.Play();
+        Invoke(nameof(SpawnObstacle), portalOpenTimer);
+    }
+    private void SpawnObstacle()
     {
         GameObject spawnedObstacle = Instantiate(obstaclePrefab, spawnPointTransform.position, Quaternion.identity);
         spawnedObstacle.GetComponent<Obstacle>().moveToTarget = moveTowardsPointTransform;
     }
+
 }
