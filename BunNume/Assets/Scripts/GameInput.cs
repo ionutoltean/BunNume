73060fc237c098f042dcd6533027c95ce3b31b30
Ioan Oltean <ionut.oltean@quadom.com>
2023-05-06 12:14:21 +0300

    now can interact

--- BunNume/Assets/Scripts/GameInput.cs
+++ BunNume/Assets/Scripts/GameInput.cs
@@ -7,26 +7,26 @@ using UnityEngine.InputSystem;
 
 public class GameInput : MonoBehaviour
 {
-    // private PlayerInputActions playernPlayerInputActions;
+     private PlayerInputActions playernPlayerInputActions;
     public event EventHandler OnInteractAction;
 
-    // private void Awake()
-    // {
-    //   //  playernPlayerInputActions = new PlayerInputActions();
-    //     playernPlayerInputActions.Enable();
-    //
-    //     playernPlayerInputActions.Player.Interact.performed += InteractPerformed;
-    // }
-    //
-    // private void InteractPerformed(InputAction.CallbackContext obj)
-    // {
-    //     OnInteractAction?.Invoke(this, EventArgs.Empty);
-    // }
+    private void Awake()
+    {
+      //  playernPlayerInputActions = new PlayerInputActions();
+        playernPlayerInputActions.Enable();
+    
+        playernPlayerInputActions.Player.Interact.performed += InteractPerformed;
+    }
+    
+    private void InteractPerformed(InputAction.CallbackContext obj)
+    {
+        OnInteractAction?.Invoke(this, EventArgs.Empty);
+    }
 
 
     public Vector2 GetMovementVectorNormalized()
     {
-        Vector2 inputVector = Vector2.down;//playernPlayerInputActions.Player.Move.ReadValue<Vector2>();
+        Vector2 inputVector = playernPlayerInputActions.Player.Move.ReadValue<Vector2>();
 
         inputVector = inputVector.normalized;
 
