a175c6799c9826939bbeff7e9d34594633782b88
Istvan Galpal <istvan.galpal@quadom.com>
2023-05-06 20:43:58 +0300

    Fixed collision on obstacle

--- BunNume/Assets/Scripts/Obstacle.cs
+++ BunNume/Assets/Scripts/Obstacle.cs
@@ -45,7 +45,7 @@ public class Obstacle : MonoBehaviour
         Destroy(gameObject);
     }
 
-    private void OnCollisionEnter2D(Collision2D col)
+    private void OnTriggerEnter2D(Collider2D col)
     {
         PlayerHealth currentPlayerHealth = col.gameObject.GetComponent<PlayerHealth>();
         if (currentPlayerHealth != null)
