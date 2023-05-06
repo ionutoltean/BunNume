19829326e81291a4d67575480815602761a230bd
ionutztasca <ionut.tasca@quadom.com>
2023-05-06 14:13:58 +0300

    added player tray

--- BunNume/Assets/Scripts/PlayerTray.cs
+++ BunNume/Assets/Scripts/PlayerTray.cs
@@ -5,30 +5,33 @@ using UnityEngine;
 public class PlayerTray : MonoBehaviour
 {
     [SerializeField] private float _updateRateInSeconds = 0.5f;
-
-    private List<Vector3> _pastPlayerPositions;
-    private Vector3 lastPostion;
+    [SerializeField] private int _positionsBufferLength = 10;
+    [SerializeField] private GameObject _toSpawnPastPosition;
+    [SerializeField] private GameObject _parentOfSpawns;
+    private List<GameObject> _pastPlayerData;
+    private Vector3 _lastPostion;
 
 
     void Start()
     {
+        Debug.Log("in start");
         Initialize();
         StartCoroutine(nameof(SavePlayerTray));
     }
 
     private void Initialize()
     {
-        _pastPlayerPositions = new List<Vector3>();
-        lastPostion = transform.position;
+        _pastPlayerData = new List<GameObject>();
+        _lastPostion = transform.localPosition;
     }
 
     private void OnDestroy()
     {
-        _pastPlayerPositions = null;
+        _pastPlayerData = null;
         StopAllCoroutines();
     }
 
-    private IEnumerable SavePlayerTray()
+    private IEnumerator SavePlayerTray()
     {
         StorePlayerPosition();
 
@@ -39,12 +42,38 @@ public class PlayerTray : MonoBehaviour
     
     private void StorePlayerPosition()
     {
-        if(lastPostion != transform.position)
+        Vector3 currentPos = transform.position;
+
+        if (_lastPostion != currentPos)
         {
-            _pastPlayerPositions.Add(transform.position);
+            _pastPlayerData.Add(SpawnEmptyGO(currentPos));
+            if (_pastPlayerData.Count > 1)
+            {
+                Debug.Log("Adding line renderer");
+                SetLineRendererPositions(_pastPlayerData[_pastPlayerData.Count - 2], _pastPlayerData[_pastPlayerData.Count - 1]);
+            }
         }
-        lastPostion = transform.position;
+        
+       
+        
+        _lastPostion = currentPos;
 
     }
     
+    private GameObject SpawnEmptyGO(Vector3 pos)
+    {
+        Debug.Log("Spawning empty GO in:"+pos);
+        GameObject go =  Instantiate(_toSpawnPastPosition,_parentOfSpawns.transform);
+        go.transform.position = pos;
+        Debug.Log("GO pos:" + go.transform);
+        return go;
+        
+    }
+    private void SetLineRendererPositions(GameObject first, GameObject second)
+    {
+        LineRenderer lineRenderer = first.GetComponent<LineRenderer>();
+        
+        lineRenderer.SetPosition(0, first.transform.position);
+        lineRenderer.SetPosition(1, second.transform.position);
+    }
 }
