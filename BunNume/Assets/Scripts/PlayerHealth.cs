9968630eb08cf60e16a79e877de0c27f2577b673
Istvan Galpal <istvan.galpal@quadom.com>
2023-05-06 18:50:52 +0300

    Added health bar

--- BunNume/Assets/Scripts/PlayerHealth.cs
+++ BunNume/Assets/Scripts/PlayerHealth.cs
@@ -1,11 +1,19 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
     [SerializeField] private float maxHealth = 100f;
     [SerializeField] private float currentHealth = 100f;
+    private Image _healthImage;
+
+    private void Awake()
+    {
+        _healthImage = GetComponentInChildren<Image>();
+    }
 
     public float GetCurrentHealth() => currentHealth;
     public void SetCurrentHealth(float newValue)
@@ -25,6 +33,9 @@ public class PlayerHealth : MonoBehaviour
         {
             currentHealth = maxHealth;
         }
+
+        _healthImage.fillAmount = currentHealth / 100;
+
     }
 
     private void Die()
